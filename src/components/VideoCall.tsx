0127f12ca3fea03048dfad0e9e3038ae29abb58b
kazisean <keh8423@nyu.edu>
2025-04-05 22:01:45 -0400

    beta

--- src/components/VideoCall.tsx
+++ src/components/VideoCall.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Button } from "@/components/ui/button";
 import { db } from '@/lib/firebase';
