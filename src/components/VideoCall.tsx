10988ee36a0001f0afffaf5f560706f18d4c4bd2
kazisean <keh8423@nyu.edu>
2025-10-01 17:31:10 -0400

    fixed clipboard color

--- src/components/VideoCall.tsx
+++ src/components/VideoCall.tsx
@@ -278,7 +278,7 @@ export function VideoCall() {
                     variant="outline" 
                     size="sm"
                     onClick={copyToClipboard}
-                    className="h-8 w-8 p-0 text-white"
+                    className="h-8 w-8 p-0 text-black"
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                       <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
